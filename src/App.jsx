5c929e5c61b6a5b1615ba7c9d142b369164e503c
jitech-code <njoyagermain777@gmail.com>
2025-02-19 17:18:44 +0100

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import HomePage from './pages/Home';
-
 import LoginPage from './pages/Login';
 import RegisterPage from './pages/Register';
 import ServicesPage from './pages/Services';
@@ -11,17 +10,15 @@ import Apropos from './pages/Apropos';
 
 function App() {
   return (
-    <Router>
-   
+    <Router basename="/cleancity">
       <Routes>
         <Route path="/" element={<HomePage />} />
-       
         <Route path="/login" element={<LoginPage />} />
-        <Route path='/register'  element={<RegisterPage/>}/>
+        <Route path='/register' element={<RegisterPage/>}/>
         <Route path="/services" element={<ServicesPage/>}/>
-        <Route path='/abonnement' element={<AbonnementPage/>}></Route>
-        <Route path='/about' element={<Apropos/>}></Route>
-        <Route path='/contact' element={<Contact/>}></Route>
+        <Route path='/abonnement' element={<AbonnementPage/>}/>
+        <Route path='/about' element={<Apropos/>}/>
+        <Route path='/contact' element={<Contact/>}/>
       </Routes>
     </Router>
   );
